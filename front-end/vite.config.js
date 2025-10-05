77862263c1ce17d5c64f5b0c40217b518996b67f
mayankkamriya <kamriyamayank45@gmail.com>
2025-10-05 22:48:06 +0530

    vite configuration fro vercel deploy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    allowedHosts: true,
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3002',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 })

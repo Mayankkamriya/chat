abb38e808798062fb31ceae2e6f067c1b4707295
mayankkamriya <kamriyamayank45@gmail.com>
2025-10-05 22:45:29 +0530

    error hanlde in DB

--- server/db.js
+++ server/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 function Connection() {
-    const MONGOURI = process.env.MONGOURI
+    const MONGOURI = process.env.MONGODB_URI || process.env.MONGOURI
+    if (!MONGOURI) {
+        console.log("MongoDB URI not found in environment variables")
+        return
+    }
     mongoose.connect(MONGOURI)
-    .then(() => console.log("connected"))
-    .catch(err => console.log(err))
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch(err => console.log("MongoDB connection error:", err))
 }
 
 module.exports = Connection
